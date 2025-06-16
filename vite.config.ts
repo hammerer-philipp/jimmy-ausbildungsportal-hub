6a78d576d0af8d0dd34b69c27033e22c8132c203
hammerer-philipp <ph@jimmy-marken.de>
2025-06-16 11:55:16 +0200

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // <--- DAS IST NEU UND ENTSCHEIDEND
   server: {
     host: "::",
     port: 8080,
